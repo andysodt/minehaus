7e5f9befc8675b62559da99c30fb9839bbc1c3f4
Andy Sodt <asodt@uw.edu>
2018-12-13 16:49:56 -0800

    less

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Minehaus
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.app_generators.stylesheet_engine :less
     config.generators do |generator|
       generator.orm :active_record, primary_key_type: :uuid
     end
