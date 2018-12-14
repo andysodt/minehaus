68079b6bb7b621f192fc6c602307d787a9449b93
Andy Sodt <asodt@uw.edu>
2018-12-13 17:08:17 -0800

    uuid

---
+++ db/migrate/20181214010028_change_primary_keys_to_uuid.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class ChangePrimaryKeysToUuid < ActiveRecord::Migration[5.2]
+  def up
+    tables = %w[
+      active_admin_comments
+      active_storage_attachments
+      active_storage_blobs
+      admin_users
+      users
+      posts
+      projects
+      tasks
+    ]
+
+    tables.each do |table|
+      remove_column table, :id
+      rename_column table, :uuid, :id
+      execute "ALTER TABLE #{table} ADD PRIMARY KEY (id);"
+    end
+  end
+end
