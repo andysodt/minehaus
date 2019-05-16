1332e117da2e290d3c8acf17172dc141a78511c2
Andy Sodt <asodt@uw.edu>
2019-05-16 11:58:19 -0700

    updates

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,16 +1,14 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
-
     field :allUsers, [UserType], null: false
 
     field :user, UserType, null: true
-      argument :id, ID, required: true
-    end
+    argument :id, ID, required: true
 
     def all_users
       User.all
     end
-
   end
-
 end
